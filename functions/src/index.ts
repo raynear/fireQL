fbb6586ec93d7a5e892a834688cb8add3d9f1950
raynear <raynear@gmail.com>
2020-02-04 19:55:13 +0900

    edit, delete, call building is checked

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -60,12 +60,16 @@ const typeDefs = gql`
   }
   type Mutation {
     addBuilding(name:String, address:String):Building
-    addRoom(buildingId:String, name:String, floor:Int):Room
+    editBuilding(buildingId:String!, name:String, address:String):Building
+    delBuilding(buildingId:String!):Building
+    addRoom(buildingId:String!, name:String, floor:Int):Room
+    editRoom(buildingId:String!, roomId:String!, name:String, floor:Int):Room
+    delRoom(buildingId:String!, roomId:String!):Room
   }
   type Query {
-    buildings: [Building]
     buildingByName(name:String): Building
-    building(id:String): Building
+    building(buildingId:String): Building
+    buildings: [Building]
     rooms(buildingId:String): [Room]
     room(buildingId:String, roomId:String): Room
   }
@@ -73,23 +77,9 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    async buildings(parent, args, context, info) {
-      const Ref = admin.database().ref("buildings");
-      const snap = await Ref.once("value");
-      const value = snap.val();
-      const RetObject = [];
-      snap.forEach((childSnap) => {
-        const aVal = childSnap.val();
-        aVal['id'] = childSnap.key;
-        RetObject.push(aVal);
-      });
-      // const test = Object.keys(value).map(key => value[key]);
-      console.log(RetObject);
-      return RetObject;
-    },
     async buildingByName(parent, args, context, info) {
-      const Ref = admin.database().ref("buildings");
-      const snap = await Ref.orderByChild('name').equalTo(args.name).once("value");
+      const ref = admin.database().ref("buildings");
+      const snap = await ref.orderByChild('name').equalTo(args.name).once("value");
       let aVal;
       snap.forEach((childSnap) => {
         aVal = childSnap.val();
@@ -97,8 +87,39 @@ const resolvers = {
       })
       return aVal;
     },
+    async building(parent, args, context, info) {
+      const ref = admin.database().ref("buildings/" + args.buildingId);
+      const snap = await ref.once("value");
+      console.log("snap", snap.key, snap.val());
+      const value = snap.val();
+      value['id'] = snap.key;
+      return value;
+    },
+    async buildings(parent, args, context, info) {
+      const ref = admin.database().ref("buildings");
+      const snap = await ref.once("value");
+      const value = snap.val();
+      const retObject = [];
+      snap.forEach((childSnap) => {
+        const aVal = childSnap.val();
+        aVal['id'] = childSnap.key;
+        retObject.push(aVal);
+      });
+      // const test = Object.keys(value).map(key => value[key]);
+      console.log(retObject);
+      return retObject;
+    },
+    async room(parent, args, context, info) {
+      const ref = admin.database().ref("buildings/" + args.buildingId + "/" + args.roomId);
+      const snap = await ref.once("value");
+      console.log("snap", snap.key, snap.val());
+      const value = snap.val();
+      console.log("value", value);
+      value['id'] = snap.key;
+      return value;
+    },
     async rooms(parent, args, context, info) {
-      const Ref = admin.database().ref("buildings/" + args.buildingId);
+      const ref = admin.database().ref("buildings/" + args.buildingId);
       // const childRef = await Ref.child("rooms");
       // const childSnap = await childRef.once("value");
       // const childValue = childSnap.val();
@@ -109,7 +130,7 @@ const resolvers = {
       //      val.forEach((childSnap) => {
       //        console.log("ASDFASDFSDF", childSnap);
       //      });
-      const snap = await Ref.once("value");
+      const snap = await ref.once("value");
       console.log("snap", snap.key, snap.val());
       const value = snap.val();
       const RetObject = [];
@@ -124,15 +145,6 @@ const resolvers = {
       console.log(RetObject);
       // get room
       return RetObject;
-    },
-    async room(parent, args, context, info) {
-      const Ref = admin.database().ref("buildings/" + args.buildingId + "/" + args.roomId);
-      const snap = await Ref.once("value");
-      console.log("snap", snap.key, snap.val());
-      const value = snap.val();
-      console.log("value", value);
-      value['id'] = snap.key;
-      return value;
     }
   },
   Mutation: {
@@ -144,9 +156,28 @@ const resolvers = {
       console.log("ret", ret);
       return ret;
     },
+    async editBuilding(parent, args, context, info) {
+      // console.log("args", args);
+      const data = { name: args.name, address: args.address };
+
+      const ref2 = admin.database().ref("buildings/" + args.buildingId);
+      await ref2.update(data);
+      const snap = await ref2.once("value");
+      const retObject = snap.val();
+      retObject['id'] = snap.key;
+      return retObject;
+
+    },
+    async delBuilding(parent, args, context, info) {
+      const ref = admin.database().ref("buildings/" + args.buildingId);
+      const res = await ref.remove()
+        .then((r: any) => { return r; })
+        .catch((e: any) => { console.log(e); });
+      return res;
+    },
     async addRoom(parent, args, context, info) {
-      const Ref = admin.database().ref("buildings/" + args.buildingId);
-      const newRef = Ref.push({ name: args.name, floor: args.floor });
+      const ref = admin.database().ref("buildings/" + args.buildingId);
+      const newRef = ref.push({ name: args.name, floor: args.floor });
       const nr = (await newRef).toString().split("/");
       console.log("newRef", nr[nr.length - 1]);
       const ret = args;
